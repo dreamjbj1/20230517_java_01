7a4706612e4f7dcee7d22c3cadc5b29b1d50fcf2
dreamjbj1 <999888@naver.com>
2023-07-11 19:56:59 +0900

    20230711
    d

--- sql/20230707_scott.sql
+++ sql/20230707_scott.sql
@@ -161,3 +161,204 @@ select * from emp
 --    order by job;
     order by 3;    
     
+    
+-- 사원명, 부서번호, 부서명, 부서위치의 조회
+select * from dept;
+select * from emp;
+select * 
+    from emp
+        join dept on emp.deptno = dept.deptno 
+; 
+select * 
+    from emp
+        join dept using (deptno)
+; 
+select ename, deptno, dname, loc 
+    from emp
+        join dept using (deptno)
+;
+
+
+select *
+from emp
+where comm is not null;
+
+select *
+from emp
+where comm is null;
+
+select *
+from emp
+where mgr is null;
+
+select *
+from emp
+order by sal asc;
+
+select *
+from emp
+order by sal desc, comm desc;
+
+select empno, ename, job, hiredate
+from emp
+order by hiredate asc;
+
+select empno, ename
+from emp
+order by empno desc;
+
+-- 부서위치가 DALLAS인 사원명, 부서번호, 부서번호, 부서명, 위치를 조회
+--select ename, dept.deptno, dname, loc
+--    from emp, dept
+
+
+
+
+select empno, loc
+    from emp cross join dept
+;
+
+select * from emp;
+select * from salgrade;
+-- 사원의 이름, 사번, sal, grade를 조회
+
+select e.ename, e.empno, e.sal ,s.grade 등급
+from emp e
+    join salgrade s on e.sal between s.losal and s.hisal
+    order by s.grade desc, e.sal desc
+;
+select * from emp;
+
+-- 자료형
+create table t1(
+    c1 char(5),
+    c2 varchar2(5)
+);
+insert into t1 values('12','12');
+commit;
+select * from t1;
+select length(c1), length(c2) from t1;
+
+desc t1;
+desc emp;
+
+-- ERD ( entity relationship diagram )
+-- UML - classDiagram, ERD 
+
+-- 오류
+select rownum, e.*  from emp e where deptno in (20, 30)
+    order by ename asc
+;
+-- 해결 방법
+select rownum, e.*  
+    from (select * from emp order by ename asc) e
+    where deptno in (20, 30)
+;
+
+-- 1page 1-3
+select rownum, e.*
+    from ( select * from emp where deptno in (20, 30) order by ename asc) e
+    where rownum between 1 and 3
+;
+-- 2page 4-6
+select rownum rnum, e.*
+    from ( select * from emp where deptno in (20, 30) order by ename asc) e
+    where rownum between 4 and 6
+    -- rnum은 select -6 수행순서로 where 절에서 사용할 수 없음.
+;
+-- 해결방법 -- rownum을 제대로 사용하기 위해서는 2개의 중첩 subquery(inline-view) 필요함.
+-- 3page 7-9
+select *
+from (select rownum rnum, e.* 
+            from ( select * from emp where deptno in (20,30) order by ename asc) e
+        )
+        where rnum between 7 and 9 
+;
+
+with abc as (select rownum rnum, e.* 
+            from ( select * from emp where deptno in (20,30) order by ename asc) e)
+select *
+from abc
+where rnum between 7 and 9
+;
+
+create view view_abc
+as 
+select rownum rnum, e.* 
+            from ( select * from emp where deptno in (20,30) order by ename asc) e
+;
+select * from view_abc;
+select *
+from view_abc
+where rnum between 7 and 9
+;
+
+
+select empno, hiredate, ename, sal
+from emp;
+where empno asc;
+
+-- 9번
+select sysdate
+from dual;
+
+-- 8번
+select empno, ename, sal
+from emp
+order by empno desc, hiredate desc;
+
+-- 10번
+select empno, ename, round(sal, -2)
+from emp
+order by sal desc;
+
+-- 11번
+select *
+from emp;
+where mod(empno,2)=1; 
+
+-- 12번 문제
+select ename, extract(year from hiredate) year, extract(month from hiredate)month
+from emp;
+
+-- 13번 문제
+select *
+from emp
+where extract(month from hiredate)='9';
+
+-- 14번 문제
+select *
+from emp
+where extract(year from hiredate)='1981';
+
+-- 15번문제 
+select *
+from emp
+where ename like '%E';
+
+-- 16번 문제
+select *
+from emp
+where ename like '__R%';
+
+--select *
+--from emp
+--where substr(ename, 3, R)
+
+-- 17번 문제
+select empno, ename, hiredate, add_months(hiredate, 480) 
+from emp;
+
+-- 18번 문제
+select *
+from emp
+where months_between(sysdate,hiredate) >= 456;
+
+-- 19번 문제
+select extract(year from sysdate) 
+from dual;
+--- 1 실습 끝
+
+-- 2 실습 시작
+
+-- 1번 문제
--- sql/20230710_kh.sql
+++ sql/20230710_kh.sql
@@ -37,6 +37,16 @@ where not EMP_NAME LIKE '이%';
 select EMP_NAME, EMAIL
 from employee
 where EMAIL LIKE '___#_%' ESCAPE '#';
+-- NULL 여부를 비교하는 연산자
+-- 관리자도 없고 부서 배치도 받지 않은 직원 조회
+SELECT EMP_NAME, MANAGER_ID, DEPT_CODE
+FROM EMPLOYEE
+where manager_id is null and dept_code is null;
+
+-- 부서 배치를 받지 않았지만 보너스를 지급받는 지원 조회
+select emp_name, bonus, dept_code
+from employee
+where dept_code is null and bonus is not null;
 
 
 -- 모든 사원들의 남, 여 , 성별과 함께 이름과 주민번호
@@ -45,6 +55,22 @@ select emp_name, emp_no,
         as "성 별"
     from employee
 ;
+-- 비교하려는 값 목록에 일치하는 값이 있으면 TRUE를 반환하는 연산자
+-- D6 부서와 D8 부서원들의 이름, 부서코드, 급여조회
+select emp_name, dept_code, salary
+from employee
+where dept_code in ('D6', 'D8');
+
+select emp_name, dept_code, salary
+from employee
+where dept_code = 'D6' or dept_code = 'D8';
+
+-- 연산자 우선순위
+-- 'J2' 또는 'J7' 직급 콛 ㅡ중 급여를 2000000보다 많이 받는 직원의 이름,급여, 직급코드 조회
+select emp_name, salary, job_code
+from employee
+where job
+
 -- java, js 삼항연산자와 비슷함
 -- string a = (substr(emp_no,8,1) == 2 ? "여" : "남";
 --if(substr(emp_no,8,1) == 2) {
@@ -64,3 +90,125 @@ select substr(emp_no,8,3) from employee;
     order by DEPT_CODE ASC;
 -- EMPLOYEE테이블에서 부서코드와 보너스 받는 사원 수 조회하고 부서코드 순으로 정렬
 -- EMPLOYEE테이블에서 성별과 성별 별 급여 평균(정수처리), 급여 합계, 인원 수 조회하고 인원수로 내림차순 정렬     
+
+-- 사원명, 부서번호, 부서명, 부서위치를 조회
+select tb1.emp_name, tb1.dept_code, tb2.dept_title, tb2.location_id, tb3.national_code, tb4.national_name
+    from employee tb1
+        join department tb2 on tb1.dept_code = tb2.dept_id
+        join location tb3 on tb2.location_id = tb3.local_code
+        join national tb4 on tb3.national_code = tb4.national_code
+            --join조건에 사용되는 칼럼명이 다르면 using 사용 불가        
+;
+
+
+
+
+select * from employee;
+select * from department;
+select * from job;
+select * from location;
+select * from national;
+select * from sal_grade;
+
+select *
+from EMPLOYEE e
+ left outer join DEPARTMENT d on e.dept_code=d.dept_id
+ ;
+ 
+ select *
+from EMPLOYEE e
+ right outer join DEPARTMENT d on e.dept_code=d.dept_id
+ ;
+
+select *
+from EMPLOYEE e
+ full outer join DEPARTMENT d on e.dept_code=d.dept_id
+ ;
+
+select * from employee;
+
+select emp_id, emp_name, job_code, salary
+from employee
+where salary >= (select avg(salary) from employee);
+
+select emp_name, job_code,dept_code,salary
+from employee
+where salary >= (select avg(salary) from employee)
+order by 2;
+
+(select avg(salary) from employee);
+
+select emp_id, emp_name, job_code,salary
+from employee
+where(job_code,salary)in(select job_code,min(salary) from employee group by job_code)
+order by 3;
+
+select emp_name, job_code,dept_code,hire_date
+from employee
+where (dept_code, job_code) in (select dept_code, job_code from employee WHERE SUBSTR(EMP_NO,8,1)=2 AND ENT_YN='Y');
+
+
+select emp_id, emp_name, job_code,salary
+from employee
+where (job_code, salary) in (select job_code, min(salary) from employee group by job_code)
+order by 3;
+
+-- 2 실습 시작
+
+-- 1번 문제
+select * from job;
+
+-- 2번 문제
+select job_name
+from job;
+
+-- 3번 문제
+select * from department;
+
+-- 4번 문제 
+select emp_name, email, phone, hire_date
+from employee;
+
+-- 5번 문제
+select hire_date, emp_name, salary
+from employee;
+
+-- 6번 문제
+--select emp_name, salary*12, 
+--from employee;
+
+-- 7번 문제
+select emp_name, salary, hire_date, phone
+from employee
+where sal_level= 'S1';
+
+-- 8번 문제
+select *
+from employee;
+
+-- 9번 문제
+select *
+from employee
+where salary >= 4000000 and job_code = 'J2';
+
+-- 10번 문제 다시풀기
+select emp_name, dept_code, hire_date
+from employee
+where (dept_code = 'D9' or dept_code='D5') and hire_date < to_date('020101', 'yymmdd');
+
+--select to_date(020101, 'yymmdd') from dual;
+
+-- 11번 문제 다시풀기 
+--select *
+--from employee
+--where hire_date >= to_date(900101,'yymmdd') and hire_date <= to_date(010101,'yymmdd');
+
+-- 12번 문제
+select emp_name
+from employee
+where emp_name like '%연';
+
+-- 13번 문제
+select emp_name, phone
+from employee
+where phone
