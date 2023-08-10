ea322aee1e9c6659a95ab1ee2335438f42355a04
dreamjbj1 <999888@naver.com>
2023-08-10 13:44:16 +0900

    mybatis

---
+++ 20230809_mybatis_servlet/src/main/java/kh/test/mybatis/common/MyBatisTemplate.java
@@ -0,0 +1,14 @@
+package kh.test.mybatis.common;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+public class MyBatisTemplate {
+//	public SqlSessionFactory getSqlSessionFactory() {
+//		String resource = "mybatis/mybatis-config.xml";
+//		SqlSessionFactory factory = null;
+//		InputStream ins = Resources.getResourceAsStream(resource);
+//	}
+}
